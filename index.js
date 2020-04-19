18aedf78cbf76134c08bd89e35d6671cedef5b11
Renan Bernardelli <renanbernardelli@gmail.com>
2020-04-19 21:34:13 +0100

    :construction: create global connection and route "products"

--- index.js
+++ index.js
@@ -17,9 +17,25 @@ app.use(bodyParser.json());
 
 //definir rotas
 const router = express.Router();
+
 router.get('/', (req, res) => res.json({message: 'Funcionando!'}));
 app.use('/', router);
+
+router.get('/products', (req, res) => {
+
+    execSQLQuery('SELECT * FROM Products', res)
+});
 
 //iniciar servidor
 app.listen(port);
 console.log('API funcionando!');
+
+//executa consultas no banco usando o pool de conexões global (conn)
+function execSQLQuery(sqlQry, res) {
+
+    global.conn.request()
+                .query(sqlQry)
+                .then(result => res.json(result.recordset))
+                .catch(err => res.json(err));
+};
+
